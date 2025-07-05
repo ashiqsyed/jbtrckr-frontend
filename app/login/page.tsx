63132191304361e1bc4bb0cce82934de7e27f98a
ashiqsyed <ahs57816@uga.edu>
2025-07-05 02:01:04 -0400

    Threw together very quick log in and sign up pages. must improve on these a LOT whenever I get to redoing the UI (likely after I confirm that the OAuth in the backend functions fully)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,73 @@
 "use client"
 
+import { Card, CardHeader, CardContent, CardFooter} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 const LoginPage = () => {
+    type LoginType = {
+        email: string,
+        password: string
+    }
+    
+    const [loginInfo, setLoginInfo] = useState<LoginType>({
+        email: "",
+        password: ""
+    })
+    const router = useRouter();
+
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {id, value} = e.target;
+        setLoginInfo(prefInfo => ({
+            ...prefInfo,
+            [id]: value
+        }))
+    };
+
+    const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (!loginInfo.email || !loginInfo.password) {
+            alert("You must enter a username and a password.")
+        } else {
+            console.log(loginInfo);
+            setLoginInfo({email: "", password: ""})
+        }
+        
+    }
+
+
     return (
-        <div>Login Page</div>
+        <div className="flex items-center justify-center w-full h-screen">
+            
+            <Card className="w-1/4">
+                <CardHeader className="text-3xl">
+                    <h1>Log In</h1>
+                </CardHeader>
+                <CardContent>
+                    <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
+                        <div className="flex flex-col gap-3 w-full">
+                            <Label htmlFor="email">Email</Label>
+                            <Input id="email" type="email" placeholder="example@email.com" onChange={handleChange} value={loginInfo.email}/>
+                        </div>
+                        <div className="flex flex-col gap-3 w-full">
+                            <Label htmlFor="password">Password</Label>
+                            <Input id="password" type="password" placeholder="password" onChange={handleChange} value={loginInfo.password}/>
+                        </div>
+                        
+                        <Button type="submit" className="w-full">Log in</Button>
+                        
+                    </form>
+                </CardContent>
+                <CardFooter className="flex items-center justify-center text-center">
+                    <p>Don't have an account? <span onClick={() => router.push("/sign-up")} className="underline underline-offset-2 text-blue-500 hover:text-blue-400 transition-all duration-150 ease-in cursor-pointer">Create one</span></p>    
+                </CardFooter>            
+            </Card>
+        </div>
     )
 }
 
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,9 +1,79 @@
 "use client"
 
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Card, CardHeader, CardContent, CardFooter} from "@/components/ui/card";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 const SignUpPage = () => {
+    type SignUpType = {
+        username: string,
+        password: string,
+        email: string
+    }
+
+    const [signUpInfo, setSignUpInfo] = useState<SignUpType>({
+        username: "",
+        password: "",
+        email: ""
+    })
+    const router = useRouter();
+
+    const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (!signUpInfo.username || !signUpInfo.password || !signUpInfo.email) {
+            alert("You must enter a username, email, and password to create an account.")
+        } else {
+            console.log(signUpInfo);
+
+            setSignUpInfo({
+                username: "",
+                password: "",
+                email: ""
+            })
+        }
+
+
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {id, value} = e.target;
+        setSignUpInfo(prevInfo => ({
+            ...prevInfo,
+            [id]: value
+        }))
+    }
+
     return (
-        <div>
-            Sign Up
+        <div className="w-full h-screen flex items-center justify-center">
+            <Card className="w-1/4">
+                <CardHeader className="text-3xl">
+                    <h1>Sign Up</h1>
+                </CardHeader>
+                <CardContent>
+                    <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
+                        <div className="flex flex-col gap-3">
+                            <Label htmlFor="username">Username</Label>
+                            <Input id="username" type="text" placeholder="clonk" value={signUpInfo.username} onChange={handleChange}/>
+                        </div>
+                        <div className="flex flex-col gap-3">
+                            <Label htmlFor="email">Email</Label>
+                            <Input id="email" type="email" placeholder="example@email.com" value={signUpInfo.email} onChange={handleChange}/>
+                        </div>
+                        <div className="flex flex-col gap-3">
+                            <Label htmlFor="password">Password</Label>
+                            <Input id="password" type="password" placeholder="password" value={signUpInfo.password} onChange={handleChange}/>
+                        </div>
+                        <Button type="submit" className="w-full">Sign Up</Button>
+                    </form>
+                </CardContent>
+                <CardFooter>
+                    <p>Already have an account? <span onClick={() => router.push("/login")}className="text-blue-500 hover:text-blue-400 transition-all duration-150 ease-in underline underline-offset-2 cursor-pointer">Log In</span></p>
+                </CardFooter>
+            </Card>
         </div>
     )
 }
